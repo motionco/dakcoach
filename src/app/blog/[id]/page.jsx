9dc0d735d901ca9363e3dbbc6c3595cdc8e1b645
motionco <motioncomc@gmail.com>
2024-03-06 23:30:00 +0900

    로컬지정

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -5,7 +5,7 @@ import { notFound } from "next/navigation";
 import sanitizeHtml from 'sanitize-html';
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts/${id}`, {
     cache: "no-store",
   });
 
