486dd3e7f136069cd7519ba964d81543c0ecb95b
motionco <motioncomc@gmail.com>
2024-03-08 15:33:40 +0900

    react

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
     domains: ["images.pexels.com", "firebasestorage.googleapis.com", "drive.google.com", "images.unsplash.com", "cdn.pixabay.com"]
   },
