5fc90d8942e611d75014f2c5ac397140202da593
motionco <motioncomc@gmail.com>
2024-03-08 18:05:13 +0900

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,9 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import sanitizeHtml from 'sanitize-html';
 import Button from "@/components/Button/Button";
 
 export const metadata = {
@@ -9,6 +12,37 @@ export const metadata = {
 };
 
 const Contact = () => {
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let name = e.target[0].value;
+    let email = e.target[1].value;
+    let content = e.target[2].value;
+
+    name = sanitizeHtml(name);
+    email = sanitizeHtml(email);
+    content = sanitizeHtml(content.replace(/\n/g, '<br/>'));
+
+    // Convert newline characters to HTML <br> tags
+    content = content.replace(/\n/g, '<br/>');
+
+    try {
+      await fetch("/api/contacts", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          email,
+          content,
+        }),
+      });
+      e.target.reset()
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>궁금한 사항이나 제안하실 것이 있다면 연락주세요.</h1>
@@ -21,7 +55,7 @@ const Contact = () => {
             className={styles.image}
           />
         </div>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <input type="text" placeholder="name" className={styles.input} />
           <input type="text" placeholder="email" className={styles.input} />
           <textarea
@@ -30,7 +64,7 @@ const Contact = () => {
             cols="30"
             rows="10"
           ></textarea>
-          <Button url="#" text="Send" />
+          <button className={styles.button}>Send</button>
         </form>
       </div>
     </div>
