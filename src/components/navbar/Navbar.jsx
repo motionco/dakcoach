449f511a5a5de000c5ca06c7c1eb51d8a91ca6dc
motionco <motioncomc@gmail.com>
2024-03-11 22:20:57 +0900

    복원

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import { signOut, useSession } from "next-auth/react";
@@ -41,18 +41,35 @@ const links = [
 
 const Navbar = () => {
   const session = useSession();
+  const [isActive, setIsActive] = useState(false); // 햄버거 메뉴 상태
+
+  const toggleMenu = () => {
+    setIsActive(!isActive); // 메뉴 토글
+  };
+
+  // 링크 클릭시 메뉴 닫기 기능 추가
+  const closeMenu = () => {
+    setIsActive(false);
+  };
 
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
         Dakcoach
       </Link>
-      <div className={styles.links}>
+      <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={`${styles.links} ${isActive ? styles.active : ''}`}>
         <DarkModeToggle />
         {links.map((link) => (
-          <Link key={link.id} href={link.url} className={styles.link}>
-            {link.title}
-          </Link>
+          <div onClick={closeMenu} key={link.id}> {/* 메뉴 닫기 기능을 위한 div 추가 */}
+            <Link key={link.id} href={link.url} className={styles.link}>
+              {link.title}
+            </Link>
+          </div>
         ))}
         {session.status === "authenticated" && (
           <button className={styles.logout} onClick={signOut}>
